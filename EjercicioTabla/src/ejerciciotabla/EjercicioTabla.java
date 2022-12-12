3ed348214fc06f0115b3edb052a5824f756878e2
Myesner <yesnermejia07@gmail.com>
2022-12-12 09:39:17 -0600

    Ejercicio

---
+++ EjercicioTabla/src/ejerciciotabla/EjercicioTabla.java
@@ -0,0 +1,18 @@
+
+package ejerciciotabla;
+
+/**
+ *
+ * @author yesner
+ */
+public class EjercicioTabla {
+
+ 
+  public static void main(String[] args) {
+     frmPrincipal op = new frmPrincipal();
+     
+     op.setVisible(true);
+     op.setLocationRelativeTo(null);
+  }
+  
+}
